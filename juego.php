789e81a24e167c973ffcea0f023bb4ec8ce568ce
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-01-10 11:52:12 +0100

    Enviando letra y manteniendo la incognita

--- juego.php
+++ juego.php
@@ -14,4 +14,10 @@ $incognita = htmlspecialchars($_POST['incognita']);
 ?>
 </h1>
 
+<form action="" method="post">
+    <input type="hidden" name="incognita" value="<?php echo $incognita ?>"
+    <label for="letra">Escriba una letra</label>
+    <input type="text" maxlength="1" size="1" name="letra" />
+    <input type="submit" value="Enviar" />
+</form>
 
