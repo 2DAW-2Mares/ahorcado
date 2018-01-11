496a6c6f723b09138a31d3bce30bcf1f153ff1bf
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-01-11 10:57:53 +0100

    almacenando las letras en un array y comparándolas todas

--- juego.php
+++ juego.php
@@ -5,25 +5,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+// var_dump($_POST);
 $incognita = htmlspecialchars($_POST['incognita']);
 $letra = isset($_POST['letra']) ? htmlspecialchars($_POST['letra']) : '';
-
+$letras = isset($_POST['letras']) ? $_POST['letras'] : array();
+$letras[] = $letra;
 ?>
 <h1>
 <?php
     for($i=0; $i<strlen($incognita); $i++){
-        if($incognita[$i] === $letra) {
-            echo $letra . " ";
-        } else {
-            echo "_ ";
+        $coincidencia = false;
+        foreach ($letras as $letra) {
+            if($incognita[$i] === $letra) {
+                echo $letra . " ";
+                $coincidencia = true;
+            } 
+        }
+        if(!$coincidencia) {
+                echo "_ ";
         }
     }
 ?>
 </h1>
 
 <form action="" method="post">
-    <input type="hidden" name="incognita" value="<?php echo $incognita ?>"
+    <input type="hidden" name="incognita" value="<?php echo $incognita ?>" />
+        <?php for($j=0; $j < count($letras) ; $j++) : ?>
+    <input type="hidden" name="letras[<?php echo $j?>]" value="<?php echo $letras[$j] ?>" />
+            <?php echo $letras[$j] ?>
+        <?php endfor; ?>
+    <br />
     <label for="letra">Escriba una letra</label>
     <input type="text" maxlength="1" size="1" name="letra" />
     <input type="submit" value="Enviar" />
