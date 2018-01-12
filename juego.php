0b3829e19d155d93dbb8778bc8d9f9e3f0d9fef9
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-01-12 09:40:10 +0100

    Evitando letras duplicadas

--- juego.php
+++ juego.php
@@ -9,7 +9,7 @@
 $incognita = htmlspecialchars($_POST['incognita']);
 $letra = isset($_POST['letra']) ? htmlspecialchars($_POST['letra']) : '';
 $letras = isset($_POST['letras']) ? $_POST['letras'] : array();
-if(preg_match("%[a-z]%", $letra))
+if(preg_match("%[a-z]%", $letra) && !in_array($letra, $letras))
     $letras[] = $letra;
 ?>
 <h1>
